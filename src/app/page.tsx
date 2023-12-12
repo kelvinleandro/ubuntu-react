186c7fed488336c6f240dd12611af0c52176189b
kelvinleandro <kelvinleandro5@gmail.com>
2023-12-12 10:12:26 -0300

    feat: NotificationCenter styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import background from '@public/22_04.jpg'
 
 export default function Home() {
   return (
-    <div className="w-full absolute bottom-0 left-0 -z-20">
-      <Image src={background} alt='Ubuntu background image' className='object-cover'/>
-    </div>
+    // <div className="w-full absolute bottom-0 left-0 -z-20">
+    //   <Image src={background} alt='Ubuntu background image' className='object-cover'/>
+    // </div>
+    <Image src={background} alt='Ubuntu background image' className='object-cover w-full h-full'/>
   )
 }
